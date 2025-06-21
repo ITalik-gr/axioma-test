25489ad48d2b1794424e9b2150767405fbc76459
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2025-06-21 16:40:21 +0300

    fix: fixing

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,11 +7,13 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Helmet>
       <title>Axios Test</title>
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <meta name="description" content="Axios Test Description" />
       <meta property="og:title" content="Axios Test" />
       <meta property="og:description" content="Axios Test Description" />
+    </Helmet>
 
     <App />
   </StrictMode>,
